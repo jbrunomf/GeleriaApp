15f69fa7ed303b8e074f5e29ed40b4ae8ca7ae64
João Bruno <jbrunomf@outlook.com>
2023-06-02 22:03:32 -0400

    Configurando imagesharp

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
+using SixLabors.ImageSharp.Web.Caching;
+using SixLabors.ImageSharp.Web.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+builder.Services.AddImageSharp(
+    options =>
+    {
+        options.BrowserMaxAge = TimeSpan.FromDays(7);
+        options.CacheMaxAge = TimeSpan.FromDays(365);
+        options.CacheHashLength = 8;
+    }).Configure<PhysicalFileSystemCacheOptions>(options =>
+    {
+        options.CacheFolder = "img/cache";
+    });
 
 var app = builder.Build();
 app.UseDeveloperExceptionPage();
+app.UseImageSharp();
 app.UseStaticFiles();
 app.UseRouting();
 app.MapDefaultControllerRoute();
