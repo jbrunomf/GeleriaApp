775daa67bf0e53dd95fdfac5e37c93190706dd17
João Bruno <jbrunomf@outlook.com>
2023-06-03 20:28:34 -0400

    Iniciando com as views/controller de galerias.

---
+++ Controllers/GaleriaController.cs
@@ -0,0 +1,22 @@
+﻿using App.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Controllers
+{
+    public class GaleriaController : Controller
+    {
+        private readonly DataContext db;
+
+        public GaleriaController(DataContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var galerias = db.Galerias.AsNoTracking().ToList();
+            return View(galerias);
+        }
+    }
+}
